18b08d52e43e5d97c95a815fffec401852174f19
HarsukritP <harsukritspall@gmail.com>
2026-04-18 09:02:13 -0400

    Initial Commit

---
+++ Pedisense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pedisense
+//
+//  Created by Harry Pall on 2026-04-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pedisense/PedisenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedisenseApp.swift
+//  Pedisense
+//
+//  Created by Harry Pall on 2026-04-18.
+//
+
+import SwiftUI
+
+@main
+struct PedisenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
